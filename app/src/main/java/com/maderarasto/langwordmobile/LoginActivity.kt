157d3dcb6e861486b47a3e65f0b26a6c5f333225
Rastislav Maděra <maderarasto@gmail.com>
2021-06-20 01:09:48 +0200

    Adds design of login layout.

--- app/src/main/java/com/maderarasto/langwordmobile/LoginActivity.kt
+++ app/src/main/java/com/maderarasto/langwordmobile/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.maderarasto.langwordmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginSubmit(view: View) {
     }
 }

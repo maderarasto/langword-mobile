62f13f2572634bcff0eb800f291c697680d647e5
Rastislav Maděra <maderarasto@gmail.com>
2021-06-19 22:25:00 +0200

    Adds fade transition between two activities.

--- app/src/main/java/com/maderarasto/langwordmobile/MainActivity.kt
+++ app/src/main/java/com/maderarasto/langwordmobile/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.maderarasto.langwordmobile
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+    }
+
+    fun onLoginClicked(view: View) {
+        val activityOptions = ActivityOptions.makeSceneTransitionAnimation(this);
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent, activityOptions.toBundle());
     }
 }

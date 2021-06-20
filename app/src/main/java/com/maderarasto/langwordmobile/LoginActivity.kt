3182bd2ba5c7a682bf35ba15e33549a4f7e7a29c
Rastislav Maděra <maderarasto@gmail.com>
2021-06-20 11:07:30 +0200

    Adds a custom toolbar with home button.

--- app/src/main/java/com/maderarasto/langwordmobile/LoginActivity.kt
+++ app/src/main/java/com/maderarasto/langwordmobile/LoginActivity.kt
@@ -1,21 +1,29 @@
 package com.maderarasto.langwordmobile
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toolbar
 import com.maderarasto.langwordmobile.views.ValidatableEdit
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
         setContentView(R.layout.activity_login)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
     }
 
     fun onLoginSubmit(view: View) {
-        val validatableEdit: ValidatableEdit = findViewById(R.id.edit_email);
+        val validatableEdit: ValidatableEdit = findViewById(R.id.edit_email)
 
-        validatableEdit.errorText = "Wrong credentials";
-        validatableEdit.showValidationError();
+        validatableEdit.errorText = "Wrong credentials"
+        validatableEdit.showValidationError()
     }
 }
